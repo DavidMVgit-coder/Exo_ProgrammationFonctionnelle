0cfa80c16b58c626a5eb9e5863ca8af932680647
DavidMVgit-coder <2141799@cegepmv.ca>
2026-02-12 11:54:57 -0500

    branch modified

--- reduce.js
+++ reduce.js
@@ -5,10 +5,26 @@ let sumOfnumbers = array.reduce((a,b) => a + b);
 console.log(sumOfnumbers)
 }
 
-sum([6,7,7])*/
+sum([6,7,7])
 
 function reduceMin(array){
     return array.reduce((a,b) => a < b ? `evens: ${a}` : `odds:${b}`);
 }
 
-console.log(reduceMin([2,3,4,5,1]))
+console.log(reduceMin([2,3,4,5,1]))*/
+
+const grades = [75, 60, 90, 80, 65, 95];
+ 
+const getMax = grades.reduce((acc, el) => Math.max(acc,el));
+console.log(getMax);
+
+const scores = { math: 85, français: 92, histoire: 78 };
+
+// Calculer la moyenne
+const moyenne = Object.entries(scores).reduce((acc, [matiere, note]) => {
+  return acc + note;
+}, 0) / Object.keys(scores).length;
+
+
+scores.forEach(element => console.log(`${matiere} : ${note} il faut beau aujourdui`));
+let test = "Le branch marche tu?"
